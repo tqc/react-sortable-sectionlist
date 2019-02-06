eb5e54f575d6dd28680fe173af299bfe8ba43009
Tom Clarkson <tom@orangeguava.com>
2019-02-06 13:24:23 +1100

    Updates

---
+++ precache-manifest.5d90157ac9531d447d1936ad4d6c61e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22d567c9eff56b3a60c6",
+    "url": "/react-sortable-sectionlist/static/js/runtime~main.22d567c9.js"
+  },
+  {
+    "revision": "28715dabaa508fedbfe9",
+    "url": "/react-sortable-sectionlist/static/js/main.28715dab.chunk.js"
+  },
+  {
+    "revision": "9247a3f169deb722f147",
+    "url": "/react-sortable-sectionlist/static/js/1.9247a3f1.chunk.js"
+  },
+  {
+    "revision": "28715dabaa508fedbfe9",
+    "url": "/react-sortable-sectionlist/static/css/main.b680607d.chunk.css"
+  },
+  {
+    "revision": "bf260486657e1e5f1beb4f4bb98c42c9",
+    "url": "/react-sortable-sectionlist/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94a93bd4c35cd2833c78215288c4edc6.js"
+  "/react-sortable-sectionlist/precache-manifest.5d90157ac9531d447d1936ad4d6c61e9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-sortable-sectionlist/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
