f41d16b8030d5fe251765779c6d2c653955523c7
Tom Clarkson <tom@orangeguava.com>
2019-02-07 12:30:24 +1100

    text fix

--- src/App.js
+++ src/App.js
@@ -21,11 +21,6 @@ class App extends Component {
                     <li>Extensible with arbitrary list item components</li>
                     <li>Multicolumn, but with the option for full width items</li>
                 </ul>
-
-
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, </p>
-                <p>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <hr/>
                 <h2>Demo</h2>
                 <p>A similar data structure rendered as a tree or grid by using different item components</p>
